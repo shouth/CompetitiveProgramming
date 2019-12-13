82065181d48f3ed535ce436f9c3aa41408f81e1b
Shota Minami <shouth373@gmail.com>
2019-12-13 14:19:34 +0900

    Add ABC038

---
+++ ABC031-040/ABC038/B_ディスプレイ.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int h1, w1, h2, w2;
+    cin >> h1 >> w1 >> h2 >> w2;
+    cout << (h1 == h2 || h1 == w2 || w1 == w2 || w1 == h2 ? "YES" : "NO") << endl;
+}
