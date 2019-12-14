3d7f9196906ed0554dcf4d763612ce2f987571e4
Shota Minami <shouth373@gmail.com>
2019-12-15 03:12:47 +0900

    Refactoring

--- ABC141-150/ABC146/A_cant_wait_for_holiday.cpp
+++ ABC141-150/ABC146/A_cant_wait_for_holiday.cpp
@@ -4,20 +4,16 @@ using namespace std;
 int main() {
     string s;
     cin >> s;
+
+    map<string, int> m {
+        { "SUN", 7 },
+        { "MON", 6 },
+        { "TUE", 5 },
+        { "WED", 4 },
+        { "THU", 3 },
+        { "FRI", 2 },
+        { "SAT", 1 },
+    };
     
-    if (s == "SUN") {
-        cout << 7 << endl;
-    } else if (s == "MON") {
-        cout << 6 << endl;
-    } else if (s == "TUE") {
-        cout << 5 << endl;
-    } else if (s == "WED") {
-        cout << 4 << endl;
-    } else if (s == "THU") {
-        cout << 3 << endl;
-    } else if (s == "FRI") {
-        cout << 2 << endl;
-    } else if (s == "SAT") {
-        cout << 1 << endl;
-    }
+    cout << m[s] << endl;
 }
