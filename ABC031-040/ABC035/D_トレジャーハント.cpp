5ce1b35935fc579ea49f3b34da685e8abf15961c
Shota Minami <shouth373@gmail.com>
2019-12-15 17:01:03 +0900

    Refactoring

--- ABC031-040/ABC035/D_トレジャーハント.cpp
+++ ABC031-040/ABC035/D_トレジャーハント.cpp
@@ -1,21 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+constexpr int64_t INF = 1e18;
 using pint = pair<int64_t, int64_t>;
 
 vector<int64_t> daijkstra(const vector<vector<pint>>& graph) {
-    vector<int64_t> dist(graph.size(), -1);
+    vector<int64_t> dist(graph.size(), INF);
     priority_queue<pint, vector<pint>, greater<pint>> pq;
     pq.emplace(0, 0);
     while (!pq.empty()) {
         int64_t cost, city;
         tie(cost, city) = pq.top(); pq.pop();
-        if (dist[city] != -1) continue;
+        if (dist[city] != INF) continue;
         dist[city] = cost;
-        for (const auto& e : graph[city]) {
-            if (dist[e.second] != -1 && dist[e.second] <= cost + e.first) continue;
-            pq.emplace(cost + e.first, e.second);
-        }
+        for (const auto& e : graph[city]) pq.emplace(cost + e.first, e.second);
     }
     return dist;
 }
@@ -36,9 +34,8 @@ int main() {
     vector<int64_t> totime = daijkstra(forg), fromtime = daijkstra(invg);
     int64_t ans = 0;
     for (int64_t i = 0; i < n; i++) {
-        if (totime[i] == -1 || fromtime[i] == -1) continue;
         int64_t stay = t - (totime[i] + fromtime[i]);
-        ans = max(ans, stay * a[i]);
+        if (stay >= 0) ans = max(ans, stay * a[i]);
     }
     cout << ans << endl;
 }
