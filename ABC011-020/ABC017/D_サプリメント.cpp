db004e4a2d92e3a45671e037bfa5ed5f8da62cc5
Minami Shota <shouth373@gmail.com>
2020-01-29 17:55:14 +0900

    Add ABC017

--- ABC011-020/ABC017/D_サプリメント.cpp
+++ ABC011-020/ABC017/D_サプリメント.cpp
@@ -1,9 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+constexpr int64_t MOD = 1e9 + 7;
+
 int main() {
     int64_t n, m;
     cin >> n >> m;
     vector<int64_t> f(n);
-    for (auto& e : f) cin >> e;
+    for (auto& e : f) cin >> e, e--;
+
+    vector<int64_t> use(m);
+    vector<int64_t> dp(n+1);
+    dp[0] = 1;
+    int64_t t = 0;
+    for (int64_t l = 0, r = 0; r < n; r++) {
+        t = (t + dp[r]) % MOD;
+        use[f[r]]++;
+        while (use[f[r]] > 1) {
+            use[f[l]]--;
+            t = (t - dp[l] + MOD) % MOD;
+            l++;
+        }
+        dp[r+1] = t;
+    }
+
+    cout << dp[n] << endl;
 }
