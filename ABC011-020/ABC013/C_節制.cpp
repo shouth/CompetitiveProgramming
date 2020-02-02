7697336580b3ac1a8c7a5de67549b4658dade226
Minami Shota <shouth373@gmail.com>
2020-02-02 18:42:23 +0900

    Add ABC013

---
+++ ABC011-020/ABC013/C_節制.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int64_t n, h;
+    cin >> n >> h;
+    int64_t a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+
+    int64_t ans = INT64_MAX;
+    for (int64_t x = 0; x <= n; x++) {
+        int64_t t = e * (n - x) - (h + x * b);
+        int64_t y = t < 0 ? 0l : t / (d + e) + 1;
+        if (n < x + y) continue;
+        ans = min(ans, a * x + c * y);
+    }
+
+    cout << ans << endl;
+}
---
+++ ABC011-020/ABC013/D_阿弥陀.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m, d;
+    cin >> n >> m >> d;
+    vector<int> c(n);
+    iota(begin(c), end(c), 0);
+    for (int i = 0; i < m; i++) {
+        int a;
+        cin >> a, a--;
+        swap(c[a], c[a+1]);
+    }
+
+    vector<int> p(n);
+    for (int i = 0; i < n; i++) p[c[i]] = i;
+
+    vector<int> ans(n, -1);
+    for (int i = 0; i < n; i++) {
+        if (ans[i] != -1) continue;
+        vector<int> cycle { i };
+        for (int j = p[i]; j != i; j = p[j]) cycle.push_back(j);
+        for (int j = 0; j < cycle.size(); j++) ans[cycle[j]] = cycle[(j + d) % cycle.size()];
+    }
+
+    for (auto& e : ans) cout << e + 1 << endl;
+}
