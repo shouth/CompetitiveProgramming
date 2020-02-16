7b0779a502009bd3af2de316ad429c0d541e856c
Minami Shota <shouth373@gmail.com>
2020-02-16 15:25:01 +0900

    Simplify

--- ABC001-010/ABC005/D_おいしいたこ焼きの焼き方.cpp
+++ ABC001-010/ABC005/D_おいしいたこ焼きの焼き方.cpp
@@ -5,13 +5,9 @@ int main() {
     int n;
     cin >> n;
     vector<vector<int>> d(n+1, vector<int>(n+1));
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            cin >> d[i][j];
-        }
-    }
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
+            cin >> d[i+1][j+1];
             d[i+1][j+1] += d[i][j+1] + d[i+1][j] - d[i][j];
         }
     }
