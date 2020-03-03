6e1fbff050830d3439b08ccbf3ffa2e144ac3d41
Minami Shota <shouth373@gmail.com>
2020-03-03 14:35:31 +0900

    Remove unneccesary file

--- ABC151-160/ABC155/tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-
-            // while (abs(nr - nl) > 1) {
-            //     i32 smid = (nl + nr) / 2;
-            //     (pow(p[smid], 2) < mid ? nl : nr) = smid;
-            // }
-            // countn = (countn + nl) / 2;
