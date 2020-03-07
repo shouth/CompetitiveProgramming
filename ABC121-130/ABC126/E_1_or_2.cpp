fd2ce5f87323268e623afd3b1608f0a4544434e8
Minami Shota <shouth373@gmail.com>
2020-03-07 17:48:41 +0900

    Solve ABC126 E

--- ABC121-130/ABC126/E_1_or_2.cpp
+++ ABC121-130/ABC126/E_1_or_2.cpp
@@ -1,11 +1,48 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> x(m), y(m), z(m);
-    for (int i=0; i<m; i++) cin >> x[i] >> y[i] >> z[i];
+using i32 = int_fast32_t;
+using i64 = int_fast64_t;
 
-    
+struct unionfind {
+    vector<i32> p, s;
+    i32 g;
+
+    unionfind(i32 n): p(n), s(n, 1), g(n) {
+        iota(begin(p), end(p), 0);
+    }
+
+    i32 find(i32 n) {
+        return p[n] == n ? n : p[n] = find(p[n]);
+    }
+
+    void unite(i32 n, i32 m) {
+        n = find(n), m = find(m);
+        if (n == m) return;
+        if (s[n] < s[m]) swap(n, m);
+        s[n] += s[m];
+        p[m] = n, g--;
+    }
+
+    i32 size(i32 n) {
+        return s[find(n)];
+    }
+
+    bool same(i32 n, i32 m) {
+        return find(n) == find(m);
+    }
+};
+
+int main() {
+    i32 n, m;
+    cin >> n >> m;
+    unionfind t(n);
+    i32 ans = 0;
+    for (i32 i = 0; i < m; i++) {
+        i32 x, y, z;
+        cin >> x >> y >> z;
+        t.unite(x, y);
+    }
+
+    cout << t.g << endl;
 }
