57ee49cd5d4d9d30f47d1f2571a1238bd5847a05
Minami Shota <shouth373@gmail.com>
2020-03-25 12:33:02 +0900

    Solve ABC141 E

--- ABC141-150/ABC141/E_who_says_a_pan.cpp
+++ ABC141-150/ABC141/E_who_says_a_pan.cpp
@@ -1,23 +1,41 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+using i32 = int_fast32_t;
+using i64 = int_fast64_t;
+
 int main() {
-    int n;
+    i32 n;
     cin >> n;
     string s;
     cin >> s;
+    
+    i32 ans = 0;
+    for (i32 l = 0; l < n; l++) {
+        i32 m = n-l;
+        vector<i32> z(m);
+        z[0] = m;
 
-    int ans = 0;
-    for (int len = 1; len <= n; len++) {
-        vector<map<string, int>> dp(n, map<string, int>());
-        dp[len - 1][s.substr(0, len)]++;
-        // cout << s.substr(0, len) << endl;
-        for (int i = len; i < n; i++) {
-            for (auto& e : dp[i-1]) dp[i][e.first] = e.second;
-            // cout << s.substr(i-len+1, len) << " ";
-            dp[i][s.substr(i-len+1, len)] = max(dp[i-len][s.substr(i-len+1, len)] + 1, dp[i][s.substr(i-len+1, len)]);
-            // cout << dp[i][s.substr(i-len+1, len)] << " " << i-len << endl;
-            if (dp[i][s.substr(i-len+1, len)] == 2) ans = len;
+        for (i32 i = 1, j = 0; i < m;) {
+            while (i+j < m && s[l+j] == s[l+i+j]) j++;
+            z[i] = j;
+
+            if (j == 0) {
+                i++;
+                continue;
+            }
+
+            i32 k = 1;
+            while (k < j && k+z[k] < j) {
+                z[i+k] = z[k];
+                k++;
+            }
+
+            i += k, j -= k;
+        }
+
+        for (i32 i = 1; i < m; i++) {
+            if (z[i] <= i) ans = max(ans, z[i]);
         }
     }
 
