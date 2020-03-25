173656a50dad92a3ea77bd64e0c9a5f29412b14c
Minami Shota <shouth373@gmail.com>
2020-03-26 07:24:03 +0900

    Solve ABC142 E

--- ABC141-150/ABC142/E_get_everything.cpp
+++ ABC141-150/ABC142/E_get_everything.cpp
@@ -1,22 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n, m;
-    cin >> n >> m;
-    vector<int> box(1 << (n-1));
-    for (int i = 0; i < m; i++) {
-        int a, b;
-        cin >> a >> b;
-        int index = 0;
-        for (int j = 0; j < b; j++) {
-            int c;
-            cin >> c;
-            index += 1 << (c-1);
-        }
+using i32 = int_fast32_t;
+using i64 = int_fast64_t;
 
-        box[index] = min(a, box[index]);
+int main() {
+    i32 n, m;
+    cin >> n >> m;
+
+    vector<i32> a(m);
+    vector<i32> c(m);
+    for (i32 i = 0; i < m; i++) {
+        i32 b;
+        cin >> a[i] >> b;
+
+        for (i32 j = 0; j < b; j++) {
+            i32 ci;
+            cin >> ci, ci--;
+            c[i] |= 1 << ci;
+        }
     }
 
-    
+    vector<i32> dp(1 << n, INT32_MAX / 2);
+    dp[0] = 0;
+    for (i32 i = 0; i < m; i++) {
+        for (i32 j = (1 << n) - 1; j >= 0; j--) {
+            dp[j | c[i]] = min(dp[j | c[i]], dp[j] + a[i]);
+        }
+    }
+
+    cout << (dp[(1 << n) - 1] == INT32_MAX / 2 ? -1 : dp[(1 << n) - 1]) << endl;
 }
