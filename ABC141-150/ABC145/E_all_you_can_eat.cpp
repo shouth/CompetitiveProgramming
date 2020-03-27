99f25574716f3991296bc5fb64534c6abd6ee231
Minami Shota <shouth373@gmail.com>
2020-03-27 14:15:21 +0900

    Solve ABC145 E

--- ABC141-150/ABC145/E_all_you_can_eat.cpp
+++ ABC141-150/ABC145/E_all_you_can_eat.cpp
@@ -1,29 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int64_t n, t;
-    cin >> n >> t;
-    using pint = pair<int64_t, int64_t>;
-    vector<pint> ab(n);
-    for (auto& e : ab) cin >> e.first >> e.second;
+using i32 = int_fast32_t;
+using i64 = int_fast64_t;
+using p32 = pair<i32, i32>;
 
-    int64_t ans = 0;
-    vector<vector<pint>> dp(n+1, vector<pint>(6010));
-    for (int64_t i = 0; i < n; i++) {
-        for (int64_t j = 0; j < 6010; j++) {
-            if (j - ab[i].first < 0) {
-                dp[i+1][j] = dp[i][j];
-            } else {
-                if (dp[i][j].first < dp[i][j-ab[i].first].first + ab[i].second) {
-                    dp[i+1][j] = make_pair(dp[i][j-ab[i].first].first + ab[i].second, j-ab[i].first);
-                    if (dp[i+1][j].second < t) ans = max(ans, dp[i+1][j].first);
-                } else {
-                    dp[i+1][j] = dp[i][j];
-                }
-            }
+int main() {
+    i32 n, t;
+    cin >> n >> t;
+    vector<p32> ab(n);
+    for (auto& e : ab) cin >> e.first >> e.second;
+    sort(begin(ab), end(ab));
+
+    vector<i32> dp(t+1);
+    for (auto& e : ab) {
+        i32 a, b;
+        tie(a, b) = e;
+        for (i32 i = t-1; i >= 0; i--) {
+            dp[min(t, i+a)] = max(dp[min(t, i+a)], dp[i] + b);
         }
     }
 
-    cout << ans << endl;
+    cout << dp[t] << endl;
 }
