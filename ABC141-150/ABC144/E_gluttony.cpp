09cdb5ebd6b0c302d51e2f0fbb9036bced9f3b92
Minami Shota <shouth373@gmail.com>
2020-03-27 10:01:17 +0900

    Solve ABC144 E

--- ABC141-150/ABC144/E_gluttony.cpp
+++ ABC141-150/ABC144/E_gluttony.cpp
@@ -1,33 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+using i32 = int_fast32_t;
+using i64 = int_fast64_t;
+
 int main() {
-    int64_t n, k;
+    i64 n, k;
     cin >> n >> k;
-    vector<int64_t> a(n);
+
+    vector<i64> a(n), f(n);
     for (auto& e : a) cin >> e;
-    vector<int64_t> f(n);
-    for (auto& e : a) cin >> e;
+    for (auto& e : f) cin >> e;
+    sort(begin(a), end(a));
+    sort(begin(f), end(f), greater<>());
 
-    sort(begin(a), end(a), greater<>());
-    sort(begin(f), end(f));
+    i64 l = -1, r = INT64_MAX / 2;
+    while (abs(r - l) > 1) {
+        i64 mid = (r + l) / 2;
 
-    vector<int64_t> laacc(n), raacc(n);
-    partial_sum(begin(a), end(a), begin(laacc));
-    partial_sum(rbegin(a), rend(a), begin(raacc));
-
-    int64_t ans = 1e18;
-    if (raacc[0] < k) {
-        int64_t bottom = 0, top = lower_bound(begin(laacc), end(laacc), k) - begin(laacc);
-
-        
-        while (bottom <= top) {
-            int64_t middle = bottom + (top - bottom) / 2;
-            
+        i64 c = 0;
+        for (i32 i = 0; i < n; i++) {
+            c += max(0l, a[i] - mid / f[i]);
         }
-    } else {
-        ans = 0;
+
+        (c > k ? l : r) = mid;
     }
 
-    cout << ans << endl;
+    cout << r << endl;
 }
