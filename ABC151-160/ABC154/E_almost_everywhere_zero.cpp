593a458c547369eee441ac727a3bef7a999b83c8
Minami Shota <shouth373@gmail.com>
2020-04-04 10:31:57 +0900

    Remove empty line

--- ABC151-160/ABC154/E_almost_everywhere_zero.cpp
+++ ABC151-160/ABC154/E_almost_everywhere_zero.cpp
@@ -16,7 +16,6 @@ i64 com(i64 m, i64 r) {
 i64 solve(i32 i, i32 j, bool less) {
     if (j == 0) return 1;
     if (i == l) return 0;
-
     if (less) return com(l-i, j) * pow(9, j);
     if (n[i] == '0') return solve(i+1, j, false);
     return solve(i+1, j, true) + solve(i+1, j-1, true) * (n[i] - '1') + solve(i+1, j-1, false);
