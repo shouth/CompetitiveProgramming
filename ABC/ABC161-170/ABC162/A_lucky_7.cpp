f282b7792eb726b95faa80acf67fc79f44f9b665
Minami Shota <shouth373@gmail.com>
2020-04-13 10:50:31 +0900

    Add ABC162

---
+++ ABC/ABC161-170/ABC162/A_lucky_7.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i32 = int_fast32_t;
+using i64 = int_fast64_t;
+
+int main() {
+    string n;
+    cin >> n;
+    cout << (n[0] == '7' || n[1] == '7' || n[2] == '7' ? "Yes" : "No") << endl;
+}
