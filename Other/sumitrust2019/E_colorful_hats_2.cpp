61e70e18ba06d3b915fc19dd80196d432dbb3d75
Minami Shota <shouth373@gmail.com>
2020-04-25 19:12:51 +0900

    Solve E and F

--- Other/sumitrust2019/E_colorful_hats_2.cpp
+++ Other/sumitrust2019/E_colorful_hats_2.cpp
@@ -1,13 +1,83 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-constexpr int64_t MOD = 1e9 + 7;
+using i64 = int_fast64_t;
+using f64 = double;
+
+template<i64 M>
+struct modint {
+    i64 x;
+
+    constexpr modint(const i64 y = 0) noexcept : x(y >= 0 ? y % M : (M - (-y) % M) % M) {}
+
+    constexpr modint operator-() const noexcept { return modint(-x); }
+    constexpr modint operator+(const modint &rhs) const noexcept { return modint(*this) += rhs; }
+    constexpr modint operator-(const modint &rhs) const noexcept { return modint(*this) -= rhs; }
+    constexpr modint operator*(const modint &rhs) const noexcept { return modint(*this) *= rhs; }
+    constexpr modint operator/(const modint &rhs) const noexcept { return modint(*this) /= rhs; }
+
+    constexpr modint &operator+=(const modint &rhs) noexcept {
+        x += rhs.x;
+        if (x >= M) x -= M;
+        return *this;
+    }
+
+    constexpr modint &operator-=(const modint &rhs) noexcept {
+        if (x < rhs.x) x += M;
+        x -= rhs.x;
+        return *this;
+    }
+
+    constexpr modint &operator*=(const modint &rhs) noexcept {
+        x = x * rhs.x % M;
+        return *this;
+    }
+
+    constexpr modint &operator/=(const modint &rhs) noexcept {
+        i64 a = rhs.x, b = M, u = 1, v = 0;
+        while (b > 0) {
+            i64 t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        *this *= u;
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &os, const modint &p) {
+        return os << p.x;
+    }
+
+    friend istream &operator>>(istream &is, modint &a) {
+        i64 t;
+        is >> t;
+        a = modint(t);
+        return is;
+    }
+};
+
+constexpr i64 MOD = 1e9 + 7;
+using m64 = modint<MOD>;
+
+void solve() {
+    i64 n;
+    cin >> n;
+    
+    m64 ans = 1;
+    i64 x = 0, y = 0, z = 0;
+    for (i64 i = 0; i < n; i++) {
+        i64 a;
+        cin >> a;
+        ans *= (x == a) + (y == a) + (z == a);
+        (x == a ? x : (y == a ? y : z))++;
+    }
+
+    cout << ans << endl;
+}
 
 int main() {
-    int64_t n;
-    cin >> n;
-    vector<int64_t> a(n);
-    for (auto& e : a) cin >> e;
-
-    
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
 }
---
+++ Other/sumitrust2019/F_interval_running.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64 = int_fast64_t;
+using f64 = double;
+
+void solve() {
+    i64 t1, t2;
+    cin >> t1 >> t2;
+    i64 a1, a2;
+    cin >> a1 >> a2;
+    i64 b1, b2;
+    cin >> b1 >> b2;
+    i64 c1 = (a1 - b1) * t1, c2 = (a2 - b2) * t2;
+
+    if (c1 + c2 == 0) {
+        cout << "infinity" << endl;
+        return;
+    }
+
+    if ((c1 ^ (c1 + c2)) >= 0) {
+        cout << 0 << endl;
+        return;
+    }
+
+    i64 r = abs(c1), d = abs(c1 + c2);
+    cout << (r / d) * 2 + (r % d != 0) << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
