f11fb51639886e7faecdbc3f90d93b4a3f74c375
Minami Shota <shouth373@gmail.com>
2020-05-01 05:23:23 +0900

    rename directories

--- Other/ddcc2020_qual/A_ddcc_finals.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main() {
-    int x, y;
-    cin >> x >> y;
-    cout << (max(4-x, 0)+max(4-y,0)) * 100000 + (x == 1 && y == 1 ? 400000 : 0) << endl;
-}
