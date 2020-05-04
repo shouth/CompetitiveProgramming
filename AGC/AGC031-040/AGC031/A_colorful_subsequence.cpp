be6017e549f3bc6d665cbc4087a7eb9f1c8e7d29
Minami Shota <shouth373@gmail.com>
2020-05-04 18:09:15 +0900

    Add AGC031

---
+++ AGC/AGC031-040/AGC031/A_colorful_subsequence.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using i64 = int_fast64_t;
+using f64 = double;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+template<i64 M>
+struct modint {
+    i64 x;
+
+    constexpr modint(const i64 y = 0) noexcept : x(y >= 0 ? y % M : (M - (-y) % M) % M) {}
+
+    constexpr modint operator-() const noexcept { return modint(-x); }
+    constexpr modint operator+(const modint &rhs) const noexcept { return modint(*this) += rhs; }
+    constexpr modint operator-(const modint &rhs) const noexcept { return modint(*this) -= rhs; }
+    constexpr modint operator*(const modint &rhs) const noexcept { return modint(*this) *= rhs; }
+    constexpr modint operator/(const modint &rhs) const noexcept { return modint(*this) /= rhs; }
+
+    constexpr modint &operator+=(const modint &rhs) noexcept {
+        x += rhs.x;
+        if (x >= M) x -= M;
+        return *this;
+    }
+
+    constexpr modint &operator-=(const modint &rhs) noexcept {
+        if (x < rhs.x) x += M;
+        x -= rhs.x;
+        return *this;
+    }
+
+    constexpr modint &operator*=(const modint &rhs) noexcept {
+        x = x * rhs.x % M;
+        return *this;
+    }
+
+    constexpr modint &operator/=(const modint &rhs) noexcept {
+        i64 a = rhs.x, b = M, u = 1, v = 0;
+        while (b > 0) {
+            i64 t = a / b;
+            swap(a -= t * b, b);
+            swap(u -= t * v, v);
+        }
+        *this *= u;
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &os, const modint &p) {
+        return os << p.x;
+    }
+
+    friend istream &operator>>(istream &is, modint &a) {
+        i64 t;
+        is >> t;
+        a = modint(t);
+        return is;
+    }
+};
+
+constexpr i64 MOD = 1e9 + 7;
+using m64 = modint<MOD>;
+
+void solve() {
+    i64 n;
+    cin >> n;
+    vector<i64> cnt(26);
+    for (i64 i = 0; i < n; i++) {
+        char c;
+        cin >> c;
+        cnt[c - 'a']++;
+    }
+
+    m64 ans = 1;
+    for (i64 i = 0; i < 26; i++) ans *= cnt[i] + 1;
+    cout << ans - 1 << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
