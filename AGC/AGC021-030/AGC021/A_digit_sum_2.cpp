02279d3994ffc64e8e21d26ceb46fdd82914dc70
Minami Shota <shouth373@gmail.com>
2020-05-19 05:53:06 +0900

    Add AGC021

---
+++ AGC/AGC021-030/AGC021/A_digit_sum_2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    i64 n;
+    cin >> n, n++;
+    i64 s = (i64) log10(n);
+    cout << 9 * s + n / (i64) pow(10, s) - 1 << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
