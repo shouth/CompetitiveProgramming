d22720495ce88a12886d8bfcf0e896358757e7e0
Minami Shota <shouth373@gmail.com>
2020-05-18 13:06:41 +0900

    Add ABC168

---
+++ ABC/ABC161-170/ABC168/B_triple_dots.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    i64 k;
+    cin >> k;
+    string s;
+    cin >> s;
+    cout << ((i64) s.size() <= k ? s : s.substr(0, k) + "...") << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
