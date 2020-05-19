131517de8ff97b27aa86da4a889f5581fdbff8e6
Minami Shota <shouth373@gmail.com>
2020-05-19 11:25:04 +0900

    Add AGC019

---
+++ AGC/AGC011-020/AGC019/B_reverse_and_compare.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    string a;
+    cin >> a;
+    i64 n = a.size();
+
+    i64 ans = n * (n - 1) / 2 + 1;
+    map<char, i64> cnt;
+    for (auto& e : a) ans -= cnt[e], cnt[e]++;
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
