00d071d721acae65782d186508e679fe0bce4b1c
Minami Shota <shouth373@gmail.com>
2020-05-19 10:05:12 +0900

    Add AGC020

---
+++ AGC/AGC011-020/AGC020/A_move_and_win.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    i64 n, a, b;
+    cin >> n >> a >> b;
+    cout << ((b - a) % 2 ? "Borys" : "Alice") << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
