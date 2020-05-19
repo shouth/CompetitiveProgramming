408c9cb17c5827f87350051906d4b0ca68db1426
Minami Shota <shouth373@gmail.com>
2020-05-19 12:08:12 +0900

    Add AGC018

---
+++ AGC/AGC011-020/AGC018/A_getting_difference.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    i64 n, k;
+    cin >> n >> k;
+    vector<i64> a(n);
+    for (auto& e : a) cin >> e;
+
+    if (*max_element(all(a)) < k) {
+        cout << "IMPOSSIBLE" << endl;
+        return;
+    }
+
+    i64 d = accumulate(all(a), 0l, __gcd<i64>);
+    if (d == 1) {
+        cout << "POSSIBLE" << endl;
+        return;
+    }
+
+    if (__gcd(d, k) == 1) {
+        cout << "IMPOSSIBLE" << endl;
+        return;
+    }
+
+    cout << "POSSIBLE" << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
