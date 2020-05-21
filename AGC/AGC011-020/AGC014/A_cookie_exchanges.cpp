a4df1810fabf9db2abcceaf4982ea1d4491fad65
Minami Shota <shouth373@gmail.com>
2020-05-21 09:19:10 +0900

    Add AGC014

---
+++ AGC/AGC011-020/AGC014/A_cookie_exchanges.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    i64 a, b, c;
+    cin >> a >> b >> c;
+
+    if (a % 2 == 0 && a == b && b == c && c == a) {
+        cout << -1 << endl;
+        return;
+    }
+
+    i64 ans = 0;
+    while (a % 2 == 0 && b % 2 == 0 && c % 2 == 0) {
+        i64 na = (b + c) / 2;
+        i64 nb = (c + a) / 2;
+        i64 nc = (a + b) / 2;
+        a = na, b = nb, c = nc;
+        ans++;
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
