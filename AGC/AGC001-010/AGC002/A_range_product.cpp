6ea28797a6385d2f25fabc292c8ce87165f5fb5b
Minami Shota <shouth373@gmail.com>
2020-05-22 12:51:19 +0900

    Add AGC002

---
+++ AGC/AGC001-010/AGC002/A_range_product.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    i64 a, b;
+    cin >> a >> b;
+    if (a > 0 && b > 0) {
+        cout << "Positive" << endl;
+    } else if (a * b <= 0) {
+        cout << "Zero" << endl;
+    } else if ((b - a + 1) % 2) {
+        cout << "Negative" << endl;
+    } else {
+        cout << "Positive" << endl;
+    }
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
---
+++ AGC/AGC001-010/AGC002/C_knot_puzzle.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    i64 n, l;
+    cin >> n >> l;
+    vector<i64> a(n);
+    for (auto& e : a) cin >> e;
+
+    i64 idx = adjacent_find(all(a), [&](i64 x, i64 y) { return x + y >= l; }) - begin(a);
+    if (idx == n) {
+        cout << "Impossible" << endl;
+        return;
+    }
+
+    cout << "Possible" << endl;
+    for (i64 i = 0; i < idx; i++) cout << i + 1 << endl;
+    for (i64 i = n - 2; i > idx; i--) cout << i + 1 << endl;
+    cout << idx + 1 << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
