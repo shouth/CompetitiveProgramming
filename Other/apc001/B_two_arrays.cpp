ce0583aa4e739e5bc35baf546b50a7dbccfa9098
Minami Shota <shouth373@gmail.com>
2020-06-05 10:34:35 +0900

    Add apc001, cf16-final, cf17-final

---
+++ Other/apc001/B_two_arrays.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    i64 n;
+    cin >> n;
+    vector<i64> a(n), b(n);
+    for (auto& e : a) cin >> e;
+    for (auto& e : b) cin >> e;
+
+    i64 ap = 0, bp = 0;
+    for (i64 i = 0; i < n; i++) {
+        if (a[i] == b[i]) continue;
+        if (a[i] > b[i]) {
+            bp += a[i] - b[i];
+        } else {
+            if ((b[i] - a[i]) % 2) {
+                bp++;
+                ap += (b[i] - a[i] + 1) / 2;
+            } else {
+                ap += (b[i] - a[i]) / 2;
+            }
+        }
+    }
+
+    cout << (bp <= ap ? "Yes" : "No") << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
