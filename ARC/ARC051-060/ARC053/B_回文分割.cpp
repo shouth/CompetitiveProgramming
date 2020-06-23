2e90ae70028401486fac6773282901946badb5fe
Minami Shota <shouth373@gmail.com>
2020-06-23 16:14:03 +0900

    Add ARC053

---
+++ ARC/ARC051-060/ARC053/B_回文分割.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    string s;
+    cin >> s;
+    map<char, i64> cnt;
+    for (auto& e : s) cnt[e]++;
+
+    i64 l = s.size();
+    i64 n = 0;
+    for (auto& [ unused, c ] : cnt) if (c % 2 == 1) n++;
+
+    cout << (n ? (l - n) / 2 / n * 2 + 1 : l) << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
