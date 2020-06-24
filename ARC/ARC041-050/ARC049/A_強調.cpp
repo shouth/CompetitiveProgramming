c5aad4a08ab492ef09db51d9cc353cfd42cd4afa
Minami Shota <shouth373@gmail.com>
2020-06-24 09:13:53 +0900

    Add ARC049

---
+++ ARC/ARC041-050/ARC049/A_強調.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+constexpr f64 EPS = DBL_EPSILON;
+
+void solve() {
+    string s;
+    cin >> s;
+    vector<i64> v(4);
+    for (auto& e : v) cin >> e;
+    reverse(all(v));
+    for (auto& e : v) s.insert(e, "\"");
+    cout << s << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
