68f4b2d5a573975487cdabd58d282eee381e9d00
Minami Shota <shouth373@gmail.com>
2020-06-27 16:06:51 +0900

    Add ARC037

---
+++ ARC/ARC031-040/ARC037/C_億マス計算.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 n, k;
+    cin >> n >> k;
+    vector<i64> a(n), b(n);
+    for (auto& e : a) cin >> e;
+    for (auto& e : b) cin >> e;
+    sort(all(a));
+    sort(all(b));
+
+    i64 l = -1, r = INF;
+    while (abs(r - l) > 1) {
+        i64 m = (l + r) / 2;
+
+        i64 cnt = 0;
+        for (auto& e : a) cnt += upper_bound(all(b), m / e) - begin(b);
+        (cnt < k ? l : r) = m;
+    }
+
+    cout << r << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
