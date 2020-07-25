d0aab88d6069816f5a5bfd1d626201b3f3a29f85
Minami Shota <shouth373@gmail.com>
2020-07-25 17:05:19 +0900

    Add ARC032

---
+++ ARC/ARC031-040/ARC032/A_ホリドッグ.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 n;
+    cin >> n;
+
+    if (n == 1) {
+        cout << "BOWWOW" << endl;
+        return;
+    }
+
+    i64 m = n * (n + 1) / 2;
+    for (i64 i = 2; i <= (i64) sqrt(m); i++) {
+        if (m % i == 0) {
+            cout << "BOWWOW" << endl;
+            return;
+        }
+    }
+
+    cout << "WANWAN" << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
---
+++ ARC/ARC031-040/ARC032/B_道路工事.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 n, m;
+    cin >> n >> m;
+    vector<vector<i64>> g(n);
+    for (i64 i = 0; i < m; i++) {
+        i64 a, b;
+        cin >> a >> b, a--, b--;
+        g[a].emplace_back(b);
+        g[b].emplace_back(a);
+    }
+
+    i64 ans = 0;
+    vector<i64> used(n);
+    for (i64 i = 0; i < n; i++) {
+        if (used[i]) continue;
+        ans++;
+
+        queue<i64> q;
+        q.emplace(i);
+        while (!q.empty()) {
+            i64 t = q.front(); q.pop();
+            used[t] = 1;
+            for (auto& e : g[t]) {
+                if (!used[e]) q.emplace(e);
+            }
+        }
+    }
+    cout << ans - 1 << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
