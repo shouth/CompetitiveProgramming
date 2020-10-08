8f2509df79953364b18e40004ce9d7ad0bb635b1
Minami Shota <shouth373@gmail.com>
2020-10-08 14:42:48 +0900

    Add ABC175

---
+++ ABC/ABC171-180/ABC175/B_making_triangle.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 n;
+    cin >> n;
+    map<i64, i64> cnt;
+    for (i64 i = 0; i < n; i++) {
+        i64 l;
+        cin >> l;
+        cnt[l]++;
+    }
+    vector<i64> keys;
+    for (auto& [ k, v ] : cnt) keys.emplace_back(k);
+    n = keys.size();
+    i64 ans = 0;
+    for (i64 i = 0; i < n; i++) {
+        for (i64 j = i + 1; j < n; j++) {
+            for (i64 k = j + 1; k < n; k++) {
+                if (keys[i] + keys[j] > keys[k]) ans += cnt[keys[i]] * cnt[keys[j]] * cnt[keys[k]];
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
---
+++ ABC/ABC171-180/ABC175/C_walking_takahashi.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+
+using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 x, k, d;
+    cin >> x >> k >> d;
+    x = abs(x);
+    if (k < x / d) {
+        x -= k * d;
+        k = 0;
+    } else {
+        k -= x / d;
+        x %= d;
+    }
+    cout << (k % 2 ? d - x : x) << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
