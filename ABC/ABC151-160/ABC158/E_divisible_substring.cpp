735c63511d1123ec28162cb14597149ae7e642ee
Minami Shota <shouth373@gmail.com>
2020-10-15 10:56:18 +0900

    Add E

--- ABC/ABC151-160/ABC158/E_divisible_substring.cpp
+++ ABC/ABC151-160/ABC158/E_divisible_substring.cpp
@@ -1,28 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-using i32 = int_fast32_t;
+#define all(x) begin(x), end(x)
+
 using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 N, P;
+    cin >> N >> P;
+    string S;
+    cin >> S;
+
+    if (P == 2 || P == 5) {
+        i64 ans = 0;
+        for (i64 i = 0; i < N; i++) {
+            if ((S[i] - '0') % P == 0) ans += i + 1;
+        }
+        cout << ans << endl;
+        return;
+    }
+
+    reverse(all(S));
+    vector<i64> cnt(P);
+    i64 rem = 0, digit = 1;
+    cnt[rem]++;
+    for (auto& e : S) {
+        rem = (rem + digit * (e - '0')) % P;
+        cnt[rem]++;
+        digit = digit * 10 % P;
+    }
+
+    i64 ans = 0;
+    for (auto& e : cnt) {
+        ans += e * (e - 1) / 2;
+    }
+    cout << ans << endl;
+}
 
 int main() {
-    i32 n, p;
-    cin >> n >> p;
-    i32 r = 1, d = 0;
-    map<int, int> rg;
-    for (i32 i = n-1; i >= 0; i--) {
-        char c;
-        cin >> c;
-        i32 e = c - '0';
-        d = (d + r * e) % p;
-        rg[d]++;
-        r = (r * 10) % p;
-    }
-
-    i32 ans = 0;
-    for (auto& e : rg) {
-        ans += e.second * (e.second - 1) / 2;
-        if (e.first == 0) ans += e.second;
-    }
-
-    cout << ans << endl;
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
 }
