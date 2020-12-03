7cd1b4da02d490f313bf98182ade5234fd2d168c
Minami Shota <shouth373@gmail.com>
2020-12-03 10:45:35 +0900

    Add AGC046

---
+++ AGC/AGC041-050/AGC046/A_takahashikum_the_strider.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+
+using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double_t;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 X;
+    cin >> X;
+    cout << lcm(X, 360) / X << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
