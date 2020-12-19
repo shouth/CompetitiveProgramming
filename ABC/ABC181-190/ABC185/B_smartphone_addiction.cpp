e3e73e95aa3851d6dc67a8ce5b479f4d98217ae3
Minami Shota <shouth373@gmail.com>
2020-12-19 19:16:35 +0900

    Add ABC185

---
+++ ABC/ABC181-190/ABC185/B_smartphone_addiction.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+
+using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double_t;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 N, M, T;
+    cin >> N >> M >> T;
+
+    i64 A, B = 0;
+    i64 remain = N;
+    for (i64 i = 0; i < 2 * M; i++) {
+        if (i % 2) {
+            cin >> B;
+            remain = min(N, remain + B - A);
+        } else {
+            cin >> A;
+            remain -= A - B;
+            if (remain <= 0) {
+                cout << "No" << endl;
+                return;
+            }
+        }
+    }
+    remain -= T - B;
+    if (remain <= 0) {
+        cout << "No" << endl;
+        return;
+    }
+    cout << "Yes" << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
---
+++ ABC/ABC181-190/ABC185/F_range_xor_query.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+
+using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double_t;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+template<typename T>
+struct fenwick_tree {
+    vector<T> t;
+    i64 n;
+
+    fenwick_tree(i64 size): t(size + 1), n(size) {}
+
+    void add(i64 i, T x) {
+        for (; i <= n; i += i & -i) t[i] ^= x;
+    }
+
+    T sum(i64 i) {
+        T res = 0;
+        for (; i > 0; i -= i & -i) res ^= t[i];
+        return res;
+    }
+};
+
+void solve() {
+    i64 N, Q;
+    cin >> N >> Q;
+
+    fenwick_tree<u64> t(N);
+    for (i64 i = 1; i <= N; i++) {
+        i64 A;
+        cin >> A;
+        t.add(i, A);
+    }
+    for (i64 i = 0; i < Q; i++) {
+        i64 T, X, Y;
+        cin >> T >> X >> Y;
+        if (T == 1) {
+            t.add(X, Y);
+        } else {
+            cout << (t.sum(Y) ^ t.sum(X - 1)) << "\n";
+        }
+    }
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
