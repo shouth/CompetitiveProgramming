717be31ee86f87c986bbd26504d6ce4301900710
Minami Shota <shouth373@gmail.com>
2021-02-14 10:55:23 +0900

    Add ABC188

---
+++ ABC/ABC181-190/ABC188/B_orthogonality.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define Yes(x) ((x) ? "Yes" : "No")
+
+using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double_t;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 N;
+    cin >> N;
+
+    vector<i64> xs(N);
+    for (auto& x : xs) cin >> x;
+    i64 ans = 0;
+    for (auto& x : xs) {
+        i64 y;
+        cin >> y;
+        ans += x * y;
+    }
+    cout << Yes(!ans) << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
