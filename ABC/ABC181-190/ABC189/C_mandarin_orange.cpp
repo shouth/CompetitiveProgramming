912275b9de689bac35298e9272fd807d0e86b06a
Minami Shota <shouth373@gmail.com>
2021-02-14 15:34:27 +0900

    Add ABC189

---
+++ ABC/ABC181-190/ABC189/C_mandarin_orange.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+
+using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double_t;
+using p64 = pair<i64, i64>;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 N;
+    cin >> N;
+    vector<i64> A(N);
+    for (auto& e : A) cin >> e;
+
+    i64 ans = 0;
+    for (i64 l = 0; l < N; l++) {
+        i64 m = INF;
+        for (i64 r = l; r < N; r++) {
+            m = min(m, A[r]);
+            ans = max(ans, m * (r - l + 1));
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
