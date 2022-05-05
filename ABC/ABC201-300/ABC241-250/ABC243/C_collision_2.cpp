b27df55106186068080d1a79f3a2679c347452ae
Shota Minami <shouth373@gmail.com>
2022-05-05 22:18:04 +0900

    Add ABC243C

---
+++ ABC/ABC201-300/ABC241-250/ABC243/C_collision_2.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include<atcoder/all>
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define yes(x) ((x) ? "yes" : "no")
+#define Yes(x) ((x) ? "Yes" : "No")
+
+using namespace std;
+using namespace atcoder;
+
+using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double_t;
+using p64 = pair<i64, i64>;
+using m64 = modint1000000007;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 N;
+    cin >> N;
+    auto XY = vector<p64>(N);
+    for (auto &[ X, Y ] : XY) cin >> X >> Y;
+    string s;
+    cin >> s;
+
+    auto yl = map<i64, i64>();
+    for (i64 i = 0; i < N; i++) {
+        if (s[i] == 'L') {
+            auto [ X, Y ] = XY[i];
+            if (yl.find(Y) == yl.end()) {
+                yl[Y] = X;
+            } else {
+                yl[Y] = max(yl[Y], X);
+            }
+        }
+    }
+    for (i64 i = 0; i < N; i++) {
+        if (s[i] == 'R') {
+            auto [ X, Y ] = XY[i];
+            if (yl.find(Y) != yl.end() && yl[Y] > X) {
+                cout << "Yes" << endl;
+                return;
+            }
+        }
+    }
+    cout << "No" << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
