edaf9c2d5a594206fec0692d2d46c7934d78d24e
Shota Minami <shouth373@gmail.com>
2022-06-17 01:24:27 +0900

    Add ABC254

---
+++ ABC/ABC201-300/ABC251-260/ABC254/C_k_sqap.cpp
@@ -0,0 +1,67 @@
+#include <utility>
+#include <bitset>
+#include <tuple>
+#include <string>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <complex>
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <atcoder/all>
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+#define yes(x) ((x) ? "yes" : "no")
+#define Yes(x) ((x) ? "Yes" : "No")
+
+using namespace std;
+using namespace atcoder;
+
+using i64 = int_fast64_t;
+using u64 = uint_fast64_t;
+using f64 = double_t;
+using p64 = pair<i64, i64>;
+using m64 = modint1000000007;
+
+constexpr i64 INF = INT_FAST64_MAX / 2;
+
+void solve() {
+    i64 N, K;
+    cin >> N >> K;
+
+    auto a = vector<i64>(N);
+    for (auto &e : a) cin >> e;
+
+    auto b = a;
+    sort(all(b));
+
+    for (i64 i = 0; i < K; i++) {
+        auto as = multiset<i64>();
+        auto bs = multiset<i64>();
+
+        for (i64 k = 0; i + k < N; k += K) {
+            as.emplace(a[i + k]);
+            bs.emplace(b[i + k]);
+        }
+
+        if (as != bs) {
+            cout << "No" << endl;
+            return;
+        }
+    }
+    cout << "Yes" << endl;
+}
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cout << fixed << setprecision(15);
+    solve();
+}
