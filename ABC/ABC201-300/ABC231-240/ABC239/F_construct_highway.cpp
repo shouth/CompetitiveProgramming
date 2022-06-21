92b9dc4fcba5fc959edece55b6ba9e33033ffab5
Shota Minami <shouth373@gmail.com>
2022-06-21 10:17:33 +0900

    Add ABC239F

--- ABC/ABC201-300/ABC231-240/ABC239/F_construct_highway.cpp
+++ ABC/ABC201-300/ABC231-240/ABC239/F_construct_highway.cpp
@@ -37,13 +37,12 @@ void solve() {
     cin >> N >> M;
     auto D = vector<i64>(N);
     for (auto &e : D) cin >> e;
-
-    auto highway = vector(N, vector<i64>());
+    auto g = vector<vector<i64>>(N);
     for (i64 i = 0; i < M; i++) {
         i64 A, B;
         cin >> A >> B, A--, B--;
-        highway[A].emplace_back(B);
-        highway[B].emplace_back(A);
+        g[A].emplace_back(B);
+        g[B].emplace_back(A);
     }
 
     if (reduce(all(D)) != 2 * (N - 1)) {
@@ -51,39 +50,58 @@ void solve() {
         return;
     }
 
-    auto build = vector<vector<i64>>();
+    auto build = vector<deque<i64>>();
     auto visited = vector<bool>(N);
-    auto q = queue<i64>();
+    auto q = deque<i64>();
     for (i64 i = 0; i < N; i++) {
         if (visited[i]) continue;
         visited[i] = 1;
-        q.emplace(i);
+        q.emplace_back(i);
 
-        auto sub = vector<i64>();
+        auto &sub = build.emplace_back();
         while (!q.empty()) {
-            auto node = q.front();
-            q.pop();
+            i64 t = q.front();
+            q.pop_front();
 
-            for (i64 j = highway[node].size(); j < D[node]; j++) {
-                sub.emplace_back(node);
+            i64 cnt = D[t] - g[t].size();
+            if (cnt < 0) {
+                cout << -1 << endl;
+                return;
             }
+            sub.insert(sub.end(), cnt, t);
 
-            for (auto next : highway[node]) {
-                if (visited[next]) continue;
-                visited[next] = 1;
-                q.emplace(next);
+            for (i64 n : g[t]) {
+                if (visited[n]) continue;
+                visited[n] = 1;
+                q.emplace_back(n);
             }
         }
-        if (sub.empty()) {
+    }
+
+    auto ord = vector<i64>(build.size());
+    iota(all(ord), 0);
+    sort(all(ord), [&](i64 a, i64 b) { return build[a].size() > build[b].size(); });
+
+    q.insert(q.end(), make_move_iterator(build[ord[0]].begin()), make_move_iterator(build[ord[0]].end()));
+    auto ans = vector<p64>();
+    for (i64 i = 1; i < build.size(); i++) {
+        if (q.empty() || build[ord[i]].empty()) {
             cout << -1 << endl;
             return;
         }
-        build.emplace_back(std::move(sub));
+        ans.emplace_back(q.front(), build[ord[i]].front());
+        q.pop_front();
+        build[ord[i]].pop_front();
+        q.insert(q.end(), make_move_iterator(build[ord[i]].begin()), make_move_iterator(build[ord[i]].end()));
     }
 
-    if (build.size() != M + 1) {
+    if (!q.empty()) {
         cout << -1 << endl;
         return;
+    }
+
+    for (auto [ a, b ] : ans) {
+        cout << a + 1 << " " << b + 1 << "\n";
     }
 }
 
